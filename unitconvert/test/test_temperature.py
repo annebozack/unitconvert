08bf7e1f0d9da4bde54ee5c8cdd42d046932c11e
annebozack <anne@Annes-MacBook-Pro.local>
2017-11-15 23:16:14 -0500

    First commit

--- unitconvert/test/test_temperature.py
+++ unitconvert/test/test_temperature.py
@@ -2,16 +2,16 @@ from unitconvert import temperature
 
 def test_fahrenheit_to_celsius():
     # 32F to C should be zero
-    assert temperature.fahrenheit_to_celsius(32) == 0
+    assert round(temperature.fahrenheit_to_celsius(32), 0) == 0
 
     # 98.6F to C should be 37
-    assert temperature.fahrenheit_to_celsius(98.6) == 37
+    assert round(temperature.fahrenheit_to_celsius(98.6), 0) == 37
 
 from unitconvert.temperature import celsius_to_fahrenheit
 
 def test_celsius_to_fahrenheit():
     # 0C to F should be 32
-    assert temperature.celsius_to_fahrenheit(0) == 32
+    assert round(temperature.celsius_to_fahrenheit(0), 0) == 32
 
     # 37C to F should be 98.6
-    assert temperature.celsius_to_fahrenheit(37) == 98.6
+    assert round(temperature.celsius_to_fahrenheit(37), 1) == 98.6
